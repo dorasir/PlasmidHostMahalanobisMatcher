0e255e193b6e204379c3352bd3dda942655f4f71
dorasir <dorasir@gmail.com>
2019-11-05 20:34:52 -0800

    Progressing in modularization

--- MahalanobisRelativeAbundance.py
+++ MahalanobisRelativeAbundance.py
@@ -7,11 +7,14 @@ NT_TYPE_NUM = 4
 
 
 class MahalanobisRelativeAbundance:
-    def __init__(self, host_path, plasmid_path, k=3):
-        self.host_path = host_path
-        self.plasmid_path = plasmid_path
+    def __init__(self, host_directory_path, plasmid_directory_path, k=3, temp_directory_path='temp_dir'):
+
+        self.host_directory_path = host_directory_path
+        self.plasmid_directory_path = plasmid_directory_path
+
         self.host_single_count = 0
         self.host_multiple_count = 0
+
         self.plasmid_single_count = 0
         self.plasmid_multiple_count = 0
 
@@ -23,24 +26,27 @@ class MahalanobisRelativeAbundance:
         for i, mer in enumerate(product(nucleotides, repeat=k)):
             self.mer_to_idx[''.join(list(mer))] = i
 
+        self.temp_directory_path = temp_directory_path
+
     @staticmethod
-    def count_single_nucleotide(path, output_folder='temp_dir'):
+    def count_single_nucleotide(path, output_directory_path='temp_dir'):
         """
         Count the number of occurrence of single nucleotides in a genome
 
         :param str path: The name of the organism, can be a fasta file or folder containing split genome
-        :param output_folder: The path to output the counting result
+        :param output_directory_path: The path to output the counting result
         :return:
         """
         if os.path.isdir(path):
-            return MahalanobisRelativeAbundance.count_single_nt_folder(path, output_folder=output_folder)
+            return MahalanobisRelativeAbundance.count_single_nt_directory(path,
+                                                                          output_directory_path=output_directory_path)
         elif os.path.isfile(path):
-            return MahalanobisRelativeAbundance.count_single_nt_file(path, output_folder=output_folder)
+            return MahalanobisRelativeAbundance.count_single_nt_file(path, output_directory_path=output_directory_path)
 
     @staticmethod
-    def count_single_nt_folder(path, save_result=True, output_folder='temp_dir'):
+    def count_single_nt_directory(path, save_result=True, output_directory_path='temp_dir'):
         genome_name = os.path.split(path)[-1]
-        nt_count_single_path = os.path.join(output_folder, genome_name + '_single_nt.npy')
+        nt_count_single_path = os.path.join(output_directory_path, genome_name + '_single_nt.npy')
         if os.path.exists(nt_count_single_path):
             occ_matrix = np.load(nt_count_single_path)
             return occ_matrix
@@ -55,9 +61,9 @@ class MahalanobisRelativeAbundance:
         return occ_matrix
 
     @staticmethod
-    def count_single_nt_file(path, save_result=True, output_folder='temp_dir'):
+    def count_single_nt_file(path, save_result=True, output_directory_path='temp_dir'):
         genome_name = os.path.split(path)[-1]
-        nt_count_single_path = os.path.join(output_folder, genome_name + '_single_nt.npy')
+        nt_count_single_path = os.path.join(output_directory_path, genome_name + '_single_nt.npy')
         if os.path.exists(nt_count_single_path):
             occ = np.load(nt_count_single_path)
             return occ
@@ -65,6 +71,70 @@ class MahalanobisRelativeAbundance:
         if save_result:
             np.save(nt_count_single_path, occ)
         return occ
+
+    def exec_jellyfish(self, fasta_file_path):
+        """
+        Runs jellyfish for a given fasta file and returns the count of each kmer
+        :param fasta_file_path:
+        :param hash_path:
+        :param jellyfish_path:
+        :return k_mer_count: a length n numpy array, where n = NT_TYPE_NUM ** k, containing the kmer count for given fasta file
+        """
+
+        hash_path = fasta_file_path + '_hash'
+
+        command = '{} count -m {} -s 200M -t 8 -o {} {}'.format(self.jellyfish_path, self.k, hash_path, fasta_file_path)
+        if not os.path.exists(hash_path + '_0'):
+            os.system(command)
+        read_hash_command = '{} dump -c {}_0'.format(self.jellyfish_path, hash_path)
+        counts = os.popen(read_hash_command).read()
+        counts = counts.split('\n')[:-1]
+        k_mer_count = np.zeros(NT_TYPE_NUM ** self.k)
+        for mer_count in counts:
+            (mer, count) = mer_count.split(' ')
+            k_mer_count[self.mer_to_idx[mer]] = count
+        return k_mer_count
+
+    def count_kmer_directory(self, directory_path, save_result=True, output_directory_path='temp_dir',
+                             overwrite_existed_result=False):
+
+        genome_name = os.path.split(directory_path)[-1]
+        kmer_count_path = os.path.join(output_directory_path, genome_name + '_kmer.npy')
+
+        if not overwrite_existed_result and os.path.exists(kmer_count_path):
+            kmer_count = np.load(kmer_count_path)
+            return kmer_count
+
+        fasta_list = os.listdir(directory_path)
+
+        kmer_count = np.zeros((len(fasta_list), NT_TYPE_NUM ** self.k))
+
+        for i, fasta_filename in enumerate(fasta_list):
+            fasta_file_path = os.path.join(directory_path, fasta_filename)
+            fasta_kmer_count = self.exec_jellyfish(fasta_file_path)
+            kmer_count[i, :] = fasta_kmer_count
+
+        if save_result:
+            np.save(kmer_count_path, kmer_count)
+
+        return kmer_count
+
+    def count_kmer_file(self, fasta_file_path, save_result=False, output_directory_path='temp_dir',
+                        overwrite_existed_result=False):
+
+        genome_name = os.path.split(fasta_file_path)[-1]
+        kmer_count_path = os.path.join(output_directory_path, genome_name + '_kmer.npy')
+
+        if not overwrite_existed_result and os.path.exists(kmer_count_path):
+            kmer_count = np.load(kmer_count_path)
+            return kmer_count
+
+        kmer_count = self.exec_jellyfish(fasta_file_path)
+
+        if save_result:
+            np.save(kmer_count_path, kmer_count)
+
+        return kmer_count
 
     @staticmethod
     def calculate_frequency_product_single(single_frequency: np.ndarray, kmer_idx, k):
@@ -101,68 +171,12 @@ class MahalanobisRelativeAbundance:
         frequency_product = np.zeros(multiple_frequency.shape)
         for i in range(multiple_frequency.shape[-1]):
             if len(multiple_frequency.shape) == 2:
-                frequency_product[:, i] = MahalanobisRelativeAbundance.calculate_frequency_product_single(single_frequency, i, k)
+                frequency_product[:, i] = MahalanobisRelativeAbundance.calculate_frequency_product_single(
+                    single_frequency, i, k)
             else:
-                frequency_product[i] = MahalanobisRelativeAbundance.calculate_frequency_product_single(single_frequency, i, k)
+                frequency_product[i] = MahalanobisRelativeAbundance.calculate_frequency_product_single(single_frequency,
+                                                                                                       i, k)
         return frequency_product
-
-    def exec_jellyfish(self, fasta_path):
-        """
-        Runs jellyfish for a given fasta file and returns the count of each kmer
-        :param fasta_path:
-        :param hash_path:
-        :param jellyfish_path:
-        :return:
-        """
-
-        hash_path = fasta_path + '_hash'
-
-        command = '{} count -m {} -s 200M -t 8 -o {} {}'.format(self.jellyfish_path, self.k, hash_path, fasta_path)
-        if not os.path.exists(hash_path + '_0'):
-            os.system(command)
-        read_hash_command = '{} dump -c {}_0'.format(self.jellyfish_path, hash_path)
-        counts = os.popen(read_hash_command).read()
-        counts = counts.split('\n')[:-1]
-        k_mer_count = np.zeros(NT_TYPE_NUM ** self.k)
-        for mer_count in counts:
-            (mer, count) = mer_count.split(' ')
-            k_mer_count[self.mer_to_idx[mer]] = count
-        return k_mer_count
-
-
-    def count_kmer_folder(self, path, save_result=True, output_folder='temp_dir'):
-        genome_name = os.path.split(path)[-1]
-
-        kmer_count_path = os.path.join(output_folder, genome_name + '_kmer.npy')
-        if os.path.exists(kmer_count_path):
-            kmer_count = np.load(kmer_count_path)
-            return kmer_count
-
-        fasta_list = os.listdir(path)
-
-        kmer_count = np.zeros((len(fasta_list), NT_TYPE_NUM ** self.k))
-
-        for fasta_filename in fasta_list:
-            fasta_path = os.path.join(path, fasta_filename)
-            fasta_kmer_count = self.exec_jellyfish(fasta_path)
-
-
-    def count_kmer_file(self, plasmid_name):
-        plasmid_name =
-        fa_path = os.path.join(plasmid_dir, plasmid_name)
-        k_mer_count = np.zeros(NT_TYPE_NUM ** k)
-        hash_path = os.path.join(plasmid_hash_dir, plasmid_name) + '_k{}'.format(k)
-        if not os.path.exists('{}_0'.format(hash_path)):
-            command = './jellyfish-linux count -m {} -s 200M -t 8 -o {} {}'.format(k, hash_path, fa_path)
-            os.system(command)
-        count = os.popen('./jellyfish-linux dump -c {}_0'.format(hash_path)).read()
-        counts = count.split('\n')[:-1]
-        for m_c in counts:
-            (m, c) = m_c.split(' ')
-            k_mer_count[self.mer_to_idx[m]] = c
-        k_mer_count_filename = os.path.join(plasmid_matrix_dir, plasmid_name) + '_k{}'.format(k)
-        np.save(k_mer_count_filename, k_mer_count)
-
 
     def calculate_relative_abundance(self, multiple_matrix, single_matrix, k):
         """
@@ -179,8 +193,5 @@ class MahalanobisRelativeAbundance:
         relative_abundance = multiple_matrix / frequency_product
         return relative_abundance
 
-
-
-    def calc_distance(self):
-        self.host_single_count = self.count_single_nucleotide(self.host_path)
-
+    def calc_distance(self, thread=1):
+        self.host_single_count = self.count_single_nucleotide(self.host_directory_path)
